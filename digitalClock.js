9776989bbea7c6033512d853fad7026d3557ed85
thatperturbedprogrammer <96286692+thatperturbedprogrammer@users.noreply.github.com>
2025-01-23 19:23:17 +0530

    Update digitalClock.js

--- digitalClock.js
+++ digitalClock.js
@@ -17,3 +17,14 @@ setInterval(updateClock, 1000);
 // const hours = now.getHours().toString().padStart(2, 0);
 // const minutes = now.getMinutes().toString().padStart(2, 0);
 // const seconds = now.getSeconds().toString().padStart(2, 0);
+
+// For AM. PM. format
+// const now = new Date();
+// let hours = now.getHours();
+// const meridiem = hours >= 12 ? "PM" : "AM";
+// hours = hours % 12 || 12;
+// hours = hours.toString().padStart(2, 0);
+// const minutes = now.getMinutes().toString().padStart(2, 0);
+// const seconds = now.getSeconds().toString().padStart(2, 0);
+
+  // const timeString = `${hours}:${minutes}:${seconds} ${meridiem}`;
